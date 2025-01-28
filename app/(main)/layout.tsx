2b28d5fed9f22bbf341adc9c025173f6571805d5
Priyansh <Priyanshyadav1012@gmail.com>
2025-01-28 08:48:59 +0530

    Dashbaord

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import SideBar from "@/components/sidebar";
 
 type Props = { children: React.ReactNode };
 
 const Layout = (props: Props) => {
   return (
     <div className="flex overflow-hidden h-screen">
+      <SideBar />
       <div className="w-full">{props.children}</div>
     </div>
   );
